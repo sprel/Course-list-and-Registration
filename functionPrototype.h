079425404cb217e72b321f26e55c084e2b59a4f7
prel <black7321@naver.com>
2020-06-06 23:52:32 +0900

    Add functionPrototype.h

---
+++ functionPrototype.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <string>
+
+using std::string;
+
+enum Day {
+	DEFAULT = 0,
+	MON = 1,
+	TUE = 2,
+	WED = 3,
+	THU = 4,
+	FRI = 5
+	//Mon / Tue / Wed / Thu / Fri
+};
+
+struct Course {
+	string courseName;  //강의명
+	string professorName;  //교수명
+	bool major;  //major or liberal art
+	int credit;  //학점
+	Day day1;  //요일
+	Day day2;  //주 2회 강의인 경우 사용
+	int startTime;  //시작시각
+	int endTime;  //마침시각
+};
+
+Course& cinCourseInfo(Course& c);
+void fwriteCoureseInfo(Course& c);
+void addCourse();
