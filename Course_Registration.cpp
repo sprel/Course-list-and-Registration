2323d2bc14ab80446dbf28ce68c5493d1e20bdf2
prel <black7321@naver.com>
2020-06-09 15:36:28 +0900

    Add Course_Registartion.cpp

---
+++ Course_Registration.cpp
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "functionPrototype.h"
+#include <iostream>
+
+using std::cout;
+
+int selectCourse() {
+	gotoxy(3, 26);
+	cout << "";
+	return 0;
+}
+
+
+void registration() {
+
+
+
+}
