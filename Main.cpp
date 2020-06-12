f50206155200c562447c8abc231636a34d485e4c
prel <black7321@naver.com>
2020-06-12 16:23:44 +0900

    Modify 메인 메뉴 UI (#39)
    resolve #39

--- Main.cpp
+++ Main.cpp
@@ -14,43 +14,102 @@ void gotoxy(int x, int y) {
 
 int selectMenu_Main() {
 
-	int x = 10;
-	int y = 3;
-	gotoxy(x, y);
-	cout << "메뉴 선택";
+	int x = 30;
+	int y = 7;
+	
+	gotoxy(x + 2, y - 3);
+	printf("<강의 시간표 조회 및 수강 신청 서비스>");
+	gotoxy(x + 12, y - 1);
+	printf("201811286 임종민");
+
+	gotoxy(x, y + 1);
+	printf("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
 	gotoxy(x, y + 2);
-	cout << "강의 시간표 조회";
+	printf("┃                                      ┃\n");
+	gotoxy(x, y + 3);
+	printf("┃                                      ┃\n");
 	gotoxy(x, y + 4);
-	cout << "강의 추가";
+	printf("┃      ┏━━━━━━━━━━━━━━━━━━━━━━━━┓      ┃\n");
+	gotoxy(x, y + 5);
+	printf("┃      ┃        강의 추가       ┃      ┃\n");
 	gotoxy(x, y + 6);
-	cout << "내 시간표 보기";
+	printf("┃      ┗━━━━━━━━━━━━━━━━━━━━━━━━┛      ┃\n");
+	gotoxy(x, y + 7);
+	printf("┃                                      ┃\n");
 	gotoxy(x, y + 8);
-	cout << "종료";
+	printf("┃      ┏━━━━━━━━━━━━━━━━━━━━━━━━┓      ┃\n");
+	gotoxy(x, y + 9);
+	printf("┃      ┃    강의 시간표 조회    ┃      ┃\n");
+	gotoxy(x, y + 10);
+	printf("┃      ┗━━━━━━━━━━━━━━━━━━━━━━━━┛      ┃\n");
+	gotoxy(x, y + 11);
+	printf("┃                                      ┃\n");
+	gotoxy(x, y + 12);
+	printf("┃      ┏━━━━━━━━━━━━━━━━━━━━━━━━┓      ┃\n");
+	gotoxy(x, y + 13);
+	printf("┃      ┃     내 시간표 보기     ┃      ┃\n");
+	gotoxy(x, y + 14);
+	printf("┃      ┗━━━━━━━━━━━━━━━━━━━━━━━━┛      ┃\n");
+	gotoxy(x, y + 15);
+	printf("┃                                      ┃\n");
+	gotoxy(x, y + 16);
+	printf("┃      ┏━━━━━━━━━━━━━━━━━━━━━━━━┓      ┃\n");
+	gotoxy(x, y + 17);
+	printf("┃      ┃          종료          ┃      ┃\n");
+	gotoxy(x, y + 18);
+	printf("┃      ┗━━━━━━━━━━━━━━━━━━━━━━━━┛      ┃\n");
+	gotoxy(x, y + 19);
+	printf("┃                                      ┃\n");
+	gotoxy(x, y + 20);
+	printf("┃                                      ┃\n");
+	gotoxy(x, y + 21);
+	printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n");
+
+
+	//gotoxy(x, y);
+	//cout << "메뉴 선택";
+	//gotoxy(x, y + 2);
+	//cout << "강의 시간표 조회";
+	//gotoxy(x, y + 4);
+	//cout << "강의 추가";
+	//gotoxy(x, y + 6);
+	//cout << "내 시간표 보기";
+	//gotoxy(x, y + 8);
+	//cout << "종료";
 
 	int key = 0;
-	int pos = y + 2;
+	int posY = y + 5;
 	int menu = 0;
 
 	while (true) {
-		gotoxy(x - 2, pos);
+		gotoxy(x + 4, posY);
+		printf("▶");
 		Sleep(200);
 		if (_kbhit()) {
 			key = _getch();
 			if (key == 13) {
-				menu = (pos - 3) / 2;
+				menu = posY / 4 - 2;
 				return menu;
 			}
 			if (key == 224) {
 				key = _getch();
 				switch (key) {
-					case 72:
-						if (pos > y) {
-							pos -= 2;
+					case UP:
+						if (posY > y + 5) {
+							gotoxy(x + 4, posY);
+							printf("  ");
+							posY = posY - 4;
+							gotoxy(x + 4, posY);
+							printf("▶");
 						}
 						break;
-					case 80:
-						if (pos < y + 8) {
-							pos += 2;
+					case DOWN:
+						if (posY < y + 17) {
+							gotoxy(x + 4, posY);
+							printf("  ");
+							posY = posY + 4;
+							gotoxy(x + 4, posY);
+							printf("▶");
 						}
 						break;
 				}
